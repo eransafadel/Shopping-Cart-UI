7dfe2e08289c043c176c0d8b30bcada670e71583
eransa <eransafadel@gmail.com>
2022-07-28 14:45:39 +0300

    fully Navbar design with styled components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Badge from "@mui/material/Badge";
 
 const Container = styled.div`
   height: 60px;
@@ -7,26 +10,78 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
 `;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const SearchContainer = styled.div`
+  border: 1px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
 const Center = styled.div`
   flex: 1;
+  text-align: center;
+`;
+
+const Logo = styled.h2`
+  font-weight: bold;
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content:flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+ 
 `;
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>left</Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Left>
+          <Language>EN </Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color:"gray",fontSize:16}}/>
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>EranSa</Logo>
+        </Center>
+        <Right>
+        <MenuItem>REGISTER</MenuItem>
+        <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
