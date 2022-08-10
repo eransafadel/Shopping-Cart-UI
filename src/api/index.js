a3ef113438078e22fdc95767245460a31f0347c2
eransafadel <eransafadel@gmail.com>
2022-08-10 16:19:37 +0300

    take token from localStorage

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 const BASE_URL ='http://localhost:5000/api/';
 
-const TOKEN = 
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTkwMjc0M2Q2ZjBiZDNkMDFjYTExOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1OTg2NTEyOSwiZXhwIjoxNjYwMTI0MzI5fQ.s6unRTZd6Aqrqm0M1HIS2aO7jTuvRt46OK4ab0dDe_g";
+// const TOKEN = 
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZTkwMjc0M2Q2ZjBiZDNkMDFjYTExOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1OTg2NTEyOSwiZXhwIjoxNjYwMTI0MzI5fQ.s6unRTZd6Aqrqm0M1HIS2aO7jTuvRt46OK4ab0dDe_g";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
